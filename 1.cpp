dbdc7daebcdc707106a89185a0118d4d4969301e
Staša Đorđević <stasadjordjevic2@gmail.com>
2026-02-27 11:23:22 +0100

    Added optimal solution

--- 1.cpp
+++ 1.cpp
@@ -7,41 +7,75 @@
 
 using namespace std;
 
-int minimumDistance(vector<vector<int>>& points) {
+int manhattanDistance(const vector<vector<int>>& points,int i,int j){
+    return abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+}
+
+pair<int,int> extremePoints(const vector<vector<int>>& points, int skipIndex){
     int n = points.size();
     
-    multiset<int> xs;
-    multiset<int> ys;
-    multiset<pair<int,int>> points_ms;
+    int max_sum=INT_MIN;
+    int min_sum=INT_MAX;
+    int max_diff=INT_MIN;
+    int min_diff=INT_MAX;
+    int max_sum_idx=-1;
+    int min_sum_idx=-1;
+    int max_diff_idx=-1;
+    int min_diff_idx=-1;
+
     for(int i=0;i<n;i++){
-        int tmp = points[i][0];
-        points[i][0] = points[i][0]-points[i][1];
-        points[i][1] = tmp+points[i][1];
-        xs.insert(points[i][0]);
-        ys.insert(points[i][1]);
-        points_ms.insert(make_pair(points[i][0], points[i][1]));
+        if(i==skipIndex) continue;
+        int sum_i = points[i][0]+points[i][1];
+        int diff_i = points[i][0]-points[i][1];
+        if(sum_i>max_sum){
+            max_sum = sum_i;
+            max_sum_idx=i;
+        }
+        if(sum_i<min_sum){
+            min_sum = sum_i;
+            min_sum_idx=i;
+        }
+        if(diff_i>max_diff){
+            max_diff = diff_i;
+            max_diff_idx=i;
+        }
+        if(diff_i<min_diff){
+            min_diff = diff_i;
+            min_diff_idx=i;
+        }
     }
-    int res = INT_MAX;
-    for(auto it:points_ms){
-        //uklanjamo it i racunamo rez kad ih uklonimo
-        auto el = xs.find(it.first);
-        if(el!=xs.end())
-            xs.erase(el);
-        el = ys.find(it.second);
-        if(el!=ys.end())
-            ys.erase(el);
-        int tmp = max(*xs.rbegin()-*xs.begin(), *ys.rbegin()-*ys.begin());
-        if(tmp<res) res=tmp;
-        xs.insert(it.first);
-        ys.insert(it.second);
-    } 
-    return res;
+    int r1 = max_sum-min_sum; //result1
+    int r2 = max_diff-min_diff; //result2
+    int point1 = max_sum_idx;
+    int point2 = min_sum_idx;
+    if(r2>r1){
+        point1=max_diff_idx;
+        point2=min_diff_idx;
+    }
+    return {point1,point2};
+}
+
+
+int minimumDistance(const vector<vector<int>>& points) {
+    int n = points.size();
+    auto res = extremePoints(points,-1);
+    int p1_idx=res.first;
+    int p2_idx=res.second;
+
+    // nasli smo max rastojanje i tacke koje ucestvuju u njemu
+    // sad gledamo uklanjanjem koje od njih dobijamo bolji (manji) rezultat
+    auto without_p1=extremePoints(points,p1_idx);
+    auto without_p2=extremePoints(points,p2_idx);
+
+    int res1 = manhattanDistance(points, without_p1.first, without_p1.second);
+    int res2 = manhattanDistance(points, without_p2.first, without_p2.second);
+    return min(res1,res2);
 }
 
 int main(){
-    // vector<vector<int>> points = {{3,10},{5,15},{10,2},{4,4}};
+    vector<vector<int>> points = {{3,10},{5,15},{10,2},{4,4}};
     // vector<vector<int>> points = {{1,1},{1,1},{1,1}};
-    vector<vector<int>> points = {{3,2},{3,9},{7,10},{4,4},{8,10},{2,7}};
+    // vector<vector<int>> points = {{3,2},{3,9},{7,10},{4,4},{8,10},{2,7}};
     cout<<minimumDistance(points)<<endl;
     return 0;
 }
